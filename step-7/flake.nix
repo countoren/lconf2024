10fc676a72ed4325c537b3371d2ad14643e233ae
Oren Rozen <orozen@carlsonsw.com>
2024-03-31 08:16:08 -0400

    add commands to flake

--- step-7/flake.nix
+++ step-7/flake.nix
@@ -1,22 +1,16 @@
-{ pkgs ? import <nixpkgs> {}
-, lib ? pkgs.lib
-, prefix ? "lconf"
-}:
-let commands = lib.fix (self: lib.mapAttrs pkgs.writeShellScript 
+{
+  outputs = { self, nixpkgs }:
+  let system = "x86_64-linux";
+  pkgs = import nixpkgs { inherit system;};
+  commands = import ./commands.nix { inherit pkgs; };
+  in
   {
-      run-py = ''
-        ${pkgs.python3}/bin/python main.py
-      '';
-      default = ''
-        nix develop -c -- ${self.run-py}
-      '';
-  });
-# try to run: nix-build commands.nix && ./result/bin/lconf-run-py 
-in pkgs.symlinkJoin rec {
-  name = prefix;
-  passthru = lib.mapAttrs (name: command: pkgs.runCommand "${prefix}-${name}" {} ''
-    mkdir -p $out/bin
-    ln -sf ${command} $out/bin/${prefix}-${name}
-    '') commands;
-  paths = lib.attrValues passthru;
+    packages.${system}.default = commands.default;
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        python3
+        commands
+      ];
+    };
+  };
 }
