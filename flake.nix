5702458d3f11d087ebd942222bc7c7f304ec8ac8
Oren Rozen <orozen@carlsonsw.com>
2024-05-05 19:56:45 -0400

    add specialized presentation vim configuration

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   {
     packages.${system} = {
       start = commands.bin.start;
+      editor = pkgs.writeShellScriptBin "editor" "nix develop -c neovide";
       default = commands;
     };
     devShells.${system}.default = pkgs.mkShell {
@@ -16,6 +17,21 @@
 
         playwright
         carbon-now-cli
+        (vims.createNvim {
+          inherit pkgs; 
+          pkgsPath = ".";
+          fontSize = "24";
+
+          # on vifm:
+          # :set viewcolumns=-{name}
+          # :set statusline=_
+          additionalVimrc =  '' 
+            let g:neovide_fullscreen = v:true
+          '';
+          additionalPlugins = with pkgs.vimPlugins; [
+
+          ];
+        })
       ];
     };
 
