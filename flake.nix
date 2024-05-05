c630121be6662f110ec6e9a5d779e31ac2af9e8c
Oren Rozen <orozen@carlsonsw.com>
2024-05-05 19:58:36 -0400

    add top level step9 client to be used by viewers

--- flake.nix
+++ flake.nix
@@ -9,20 +9,23 @@
   let system = "x86_64-linux";
   pkgs = import nixpkgs { inherit system;};
   commands = import ./commands.nix { inherit pkgs; };
+  step9Commands = import ./step-9/commands.nix { inherit pkgs; serverIp = "localhost"; };
   in
   {
     packages.${system} = {
+      client = pkgs.writeShellScriptBin "client" ''
+        ${step9Commands.set.server-get} | ${import ./step-9/rust-project { inherit pkgs; }}/bin/rust-cli
+      '';
       start = commands.bin.start;
       editor = pkgs.writeShellScriptBin "editor" "nix develop -c neovide";
       default = commands;
     };
+
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         reveal-md
         commands
 
-        playwright
-        carbon-now-cli
         (vims.createNvim {
           inherit pkgs; 
           pkgsPath = ".";
